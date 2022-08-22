7652c22082b94f9c7f218b95294b1469e5579069
enmanuel <enmanuel.lassis@higherme.com>
2022-08-22 16:59:55 -0400

    add components folder

---
+++ src/components/login.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Login = () => {
+
+    return (
+        <form>
+            <input name="user" />
+            <input name="password" />
+            <button>Login</button>
+        </form>
+    )
+
+}
+
+export default Login;
