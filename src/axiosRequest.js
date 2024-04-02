9732dd5591e64d7071d69093656dddaa6083d541
enmanuel <enmanuel.lassis@higherme.com>
2024-04-02 18:20:36 -0400

    Add environment conditional to axios

--- src/axiosRequest.js
+++ src/axiosRequest.js
@@ -1,19 +1,22 @@
-import axios from "axios"
+import axios from "axios";
 import tough from "tough-cookie";
 
 const { CookieJar } = tough;
 
 const cookieJar = new CookieJar(undefined, {
   allowSpecialUseDomain: true,
-  rejectPublicSuffixes: false
+  rejectPublicSuffixes: false,
 });
 
 const http = axios.create({
-  baseURL:'http://localhost:8080/technicians/public',
-  headers:{
-    'X-Requested-With': 'XMLHttpRequest', 
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? process.env.REACT_APP_API_KEY
+      : process.env.REACT_APP_PROD_API_KEY,
+  headers: {
+    "X-Requested-With": "XMLHttpRequest",
   },
-  withCredentials:true,
+  withCredentials: true,
 });
 
 http.defaults.jar = cookieJar;
